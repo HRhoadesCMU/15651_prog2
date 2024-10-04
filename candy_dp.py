2a29763ffccfe9eacfb92faab8093c882f5bf96f
HRhoadesCMU <hrhoades@andrew.cmu.edu>
2024-10-04 18:01:06 -0400

    Functional Range Sum
    Does not properly handle duplicate values

--- candy_dp.py
+++ candy_dp.py
@@ -1,13 +1,7 @@
 import sys
 max_val_cap = 1000000007 # final result is total value % max_val_cap
 
-#optimal substructure of a valid substring: longest sequence to current index
-# with a max value less than the value at the current index
 """
-Choice: take house or pass
-- take house: add value to total and increase max val seen to item val, val of k decreases
-- don't take house: no change in value or max val, index increases
-
 - segTree with range query gives max substring to index
 - if equal/longer than k -> (val choose k) options, else next
 """
@@ -19,16 +13,16 @@ def build_segTree(size):
     return tree
 
 def parent(index):
-    return (index-1)/2
+    return int((index-1)/2)
 
 def left_child(index):
-    return index*2 + 1
+    return int(index*2 + 1)
 
 def right_child(index):
-    return index*2 + 2
+    return int(index*2 + 2)
 
 def assign(st, index, value):
-    current_node = index + (len(st)/2) - 1
+    current_node = index + int((len(st)/2) - 1)
     st[current_node] = value
     while current_node > 0:
         current_node = parent(current_node)
@@ -36,13 +30,15 @@ def assign(st, index, value):
     return st
 
 def range_query(st, current_node, left_query, right_query, left_edge, right_edge):
-    if (left_query <= left_edge) and (right_query >= right_edge ):
+    if ((left_query <= left_edge) and (right_query >= right_edge)):
         return st[current_node]
+    elif (left_query > right_edge) or (right_query < left_edge):
+        return 0
     else:
-        mid_point = (left_edge + right_edge) / 2
+        mid_point = int((left_edge + right_edge) / 2)
         #exclude left
         if (left_query >= mid_point):
-            return range_query(st, right_child(current_node), left_query, right_query, mid_point, right_edge)
+            return range_query(st, right_child(current_node), left_query, right_query, mid_point + 1, right_edge)
         #exclude right
         elif (right_query <= mid_point):
             return range_query(st, left_child(current_node), left_query, right_query, left_edge, mid_point)
@@ -52,7 +48,8 @@ def range_query(st, current_node, left_query, right_query, left_edge, right_edge
             right = range_query(st, right_child(current_node), left_query, right_query, mid_point, right_edge)
             return left + right
 
-#def longest_increasing_subset(st, )
+def range_sum(st, i, j):
+    return range_query(st, 0, i, j, 0, int(len(st)/2))
 
 def map_values(unsorted_house_list):
     value_map = {}
@@ -75,7 +72,11 @@ if __name__ == "__main__":
     houses = input().split()
     segTree_houses = build_segTree(len(houses))
     mapped_houses = map_values(houses)
+    lis_table = [0 for i in range(0, len(houses))]
     print(mapped_houses)
-    #sorted_houses = houses
-    #sorted_houses.sort()
-    #mapped_houses = map_values(sorted_houses)
+    #rangeSum 0 to index
+    for keys in mapped_houses:
+        for indices in mapped_houses[keys]:
+            lis_table[indices] = range_sum(segTree_houses, 0, indices) + 1
+            assign(segTree_houses, indices, 1)
+    print(lis_table)
